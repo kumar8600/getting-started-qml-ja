329eb58fe39785414a92c323b727614753a9acbb
kumar8600 <kumar1@hotmail.co.jp>
2014-04-05 22:43:46 +0900

    Removed index and changed master for getting-...
    and add README

---
+++ README.rst
@@ -0,0 +1,8 @@
+======================================================
+ Getting started QML programming Japanese translation
+======================================================
+
+Available at http://getting-started-qml-ja.readthedocs.org/ .
+
+.. note::
+   I'm waiting your issue report and pull-request in this repository!!!
--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'getting-started-qml-ja'
 
 # General information about the project.
 project = 'Getting started QML programming Japanese translation'
@@ -197,7 +197,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'GettingstartedQMLprogrammingJapanesetranslation.tex', 'Getting started QML programming Japanese translation Documentation',
+  ('getting-started-qml-ja', 'GettingstartedQMLprogrammingJapanesetranslation.tex', 'Getting started QML programming Japanese translation Documentation',
    'Digia Plc and/or its subsidiary(-ies) and kumar8600', 'manual'),
 ]
 
@@ -227,7 +227,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'gettingstartedqmlprogrammingjapanesetranslation', 'Getting started QML programming Japanese translation Documentation',
+    ('getting-started-qml-ja', 'gettingstartedqmlprogrammingjapanesetranslation', 'Getting started QML programming Japanese translation Documentation',
      ['Digia Plc and/or its subsidiary(-ies) and kumar8600'], 1)
 ]
 
@@ -241,7 +241,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'GettingstartedQMLprogrammingJapanesetranslation', 'Getting started QML programming Japanese translation Documentation',
+  ('getting-started-qml-ja', 'GettingstartedQMLprogrammingJapanesetranslation', 'Getting started QML programming Japanese translation Documentation',
    'Digia Plc and/or its subsidiary(-ies) and kumar8600', 'GettingstartedQMLprogrammingJapanesetranslation', 'One line description of project.',
    'Miscellaneous'),
 ]
